e18c89f402b165c48eb2bc6a06d4a26547986e22
Ramon Smits <ramon.smits@gmail.com>
2020-06-11 23:54:00 +0200

    Added test project.

---
+++ src/MqttUdp/Packets/PublishPacketExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace MqttUdp
+{
+    public static class PublishPacketExtensions
+    {
+        public static string ReadString(this PublishPacket instance)
+        {
+            return Encoding.UTF8.GetString(instance.Payload);
+        }
+        public static void Write(this PublishPacket instance, string value)
+        {
+            instance.Payload = Encoding.UTF8.GetBytes(value);
+        }
+    }
+}
